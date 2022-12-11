3fd406ebfab796b3276a7e8e4cfb45feac3b0f45
Eyal Halfon <eyalh@nimbleway.com>
2022-12-11 15:09:45 +0200

    11

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@
 set -e
 set -x
 set -v
- CLUSTERS=$(doctl kubernetes cluster list --format Name,Tags |grep ${CLUSTER_NAME} |cut -d ' ' -f1)
-
- IFS=$'\n' read -r -d '' -a MODIFY <<< $CLUSTERS
-  
- RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
- echo $RESULT
- echo "names=$(echo $RESULT)" >> $GITHUB_OUTPUT
- exit 0
+CLUSTERS=$(doctl kubernetes cluster list --format Name,Tags |grep ${CLUSTER_NAME} |cut -d ' ' -f1)
+IFS=$'\n' read -r -d '' -a MODIFY <<< $CLUSTERS
+RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
+echo $RESULT
+echo "names=$(echo $RESULT)" >> $GITHUB_OUTPUT
+exit 0
