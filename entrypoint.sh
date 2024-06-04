41cd33e52e3b2a38ca2c365a31e004cde0294f2c
Eyal Halfon <eyalh@nimbleway.com>
2024-06-04 08:35:37 +0300

    revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ if [ $GET_ALL = true ]; then
   IFS=$'\n' read -r -d '' -a MODIFY <<< $CLUSTERS
   RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
   echo $RESULT
-  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+  echo "names=$(echo $RESULT)" >> $GITHUB_OUTPUT
 else
   CLUSTERS=$(doctl kubernetes cluster list --no-header --format Name,Tags | grep ${ENVIRONMENT} |  awk '{print $1}' | grep ${CLUSTER_NAME} |cut -d ' ' -f1)
   IFS=$'\n' read -r -d '' -a MODIFY <<< $CLUSTERS
   RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
   echo $RESULT
-  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+  echo "names=$(echo $RESULT)" >> $GITHUB_OUTPUT
 fi
 
